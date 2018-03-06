bb82a13456bccd2dfef76f36c29d528f39410fb2
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-05 18:20:54 -0700

    heroku changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var htmlRoutes = require("./app/routing/htmlRoutes.js");
 var apiRoutes = require("./app/routing/apiRoutes.js");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 
 
