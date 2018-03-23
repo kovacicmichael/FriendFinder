daa2d6f6e52f2cba5c686c327b3f90ca24fa6624
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-22 20:28:08 -0600

    improvements made, complete survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,12 +4,12 @@ var friendList = [
 	{
 		name: "William",
 		photo: "https://typeset-beta.imgix.net/rehost/2016/9/13/397f5ccf-20c4-471b-a86a-05c5eb051966.jpg?w=740&h=444&fit=crop&crop=faces&auto=format&q=70",
-		"choices[]": [1, 4, 5, 2, 3]
+		"choices[]": [1, 4, 5, 2, 3, 4, 5, 2, 1, 3]
 	},
 	{
 		name: "Black Widow",
 		photo: "https://www.themarysue.com/wp-content/uploads/2016/10/black-widow-533x800-1.jpg",
-		"choices[]": [3, 2, 4, 1, 4]
+		"choices[]": [3, 2, 4, 1, 4, 3, 2, 4, 5, 1]
 	}
 
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,44 +12,65 @@ var friendList = require("../data/friends.js")
   // API GET/POST Requests
   // ---------------------------------------------------------------------------
 	router.post("/friends", function(req, res) {
-
-		var user1 = req.body["choices[]"];
 		var match;
-
-		console.log(user1)
-		console.log("----------------")
+		//the maximum difference possible between two users is the starting difference
 		var diffTotal = 40;
+		let differenceArray = [];
 
+		//a for loop to loop through and compare the newUser's scores to those of the past users
 	  	for(var i = 0; i < friendList.length; i++){
-	  		var user2 = friendList[i]["choices[]"];
-	  		
-		  	function difference(user1, user2){
-		  		var diffOne = Math.abs(user1[0] - user2[0]);
-		  		var diffTwo = Math.abs(user1[1] - user2[1]);
-		  		var diffThree = Math.abs(user1[2] - user2[2]);
-		  		var diffFour = Math.abs(user1[3] - user2[3]);
-		  		var diffFive = Math.abs(user1[4] - user2[4]);
-
-		  		newDiffTotal = diffOne + diffTwo + diffThree + diffFour + diffFive
-		  		//console.log(newDiffTotal)
-		  		if(newDiffTotal < diffTotal){
-		  			diffTotal = newDiffTotal;
-		  			match = friendList[i];
+	  		var pastUsers = friendList[i]["choices[]"];
+	  		console.log("user2 = " + pastUsers)
+	  		//user who submitted the survey
+	  		var newUser = req.body["choices[]"];
+	  		console.log("user1 = " + newUser)
+	  		//you need to clear the array before you start the next loop comparing the next past user's score
+	  		differenceArray = [];
+	  		//this for loop will loop through and compare each of the choices that the new and past users inputed
+		  		for(let j = 0; j < pastUsers.length; j++){
+		  			let difference = Math.abs(newUser[j] - pastUsers[j]);
+		  			console.log("User 1 " + newUser[j])
+		  			console.log("User 2 " + pastUsers[j])
+		  			differenceArray.push(difference)
 		  		}
+		  	//gets the sum of all the integers within the array
+		  	console.log("array of diff: " + differenceArray)
+		  	function getSum (total, num){
+		  		return total + num;
 		  	}
-		  	difference(user1, user2);
+		  	newDiffTotal = differenceArray.reduce(getSum)
+		  	//sets the new user's match to the past user with the least amount of difference between their answers
+		  	if(newDiffTotal < diffTotal){
+		  		diffTotal = newDiffTotal;
+		  		match = friendList[i];
+		  	}
+		  	console.log("total diff: " + diffTotal)
 	  	}
-
+	  	console.log(diffTotal)
 	  	friendList.push(req.body);
 	  	res.json(match);
-
 	});
 
 	router.get("/friends", function(req, res){
-		//console.log("works")
+		//complete list of all user submition results
 		res.json(friendList);
 	});
 
 
 module.exports = router;
+
+//Another way to calculate the difference, a little easier to read, but more code...
+
+
+			// function difference(user1, user2){
+				// var diffOne = Math.abs(user1[0] - user2[0]);
+		  		// var diffTwo = Math.abs(user1[1] - user2[1]);
+		  		// var diffThree = Math.abs(user1[2] - user2[2]);
+		  		// var diffFour = Math.abs(user1[3] - user2[3]);
+		  		// var diffFive = Math.abs(user1[4] - user2[4]);
+
+		  		// newDiffTotal = diffOne + diffTwo + diffThree + diffFour + diffFive
+			// }
+		  	//difference(user1, user2);
+
 
